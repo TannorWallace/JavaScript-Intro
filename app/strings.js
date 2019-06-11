ab50c151d7014af4a25d11a94633c70279eba616
TannorWallace <tannor.wallace@gmail.com>
2019-06-11 15:35:08 -0600

    pushing progress in to the yellow...confused area..

--- app/strings.js
+++ app/strings.js
@@ -1,8 +1,8 @@
 // 1. Given the following sentence: Change all of the lowercase letters i, in reference to yourself, 
 // to the uppercase I careful not to uppercase the i in the word pair
 let sentence = 'When i went to the mall i bought a pair of shoes.';
-let word = "I"
-function capitalizeI(str) {
+
+function uppercase(str) {
   str = str.split(" ");
   for (let index = 0; index < str.length; index++) {
     let word = str[index];
@@ -25,15 +25,16 @@ function capitalizeI(str) {
 let jsHistory = 'JavaScript, not to be confused with Java, was created in 10 days in May 1995 by Brendan Eich, then working at Netscape and now of Mozilla. JavaScript was not always known as JavaScript: the original name was Mocha, a name chosen by Marc Andreessen, founder of Netscape. In September of 1995 the name was changed to LiveScript, then in December of the same year, upon receiving a trademark license from Sun, the name JavaScript was adopted. This was somewhat of a marketing move at the time, with Java being very popular around then.';
 
 function letterCount(str, letter) {
+
   let count = 0;
   for (let i = 0; i < str.length; i++) {
-    if (str[i] == letter.toLowerCase() || letter.toUpperCase()) {
+    if (str[i].toLowerCase() == letter.toLowerCase()) {
       count++;
     }
   }
   return count;
 }
-letterCount(jsHistory, "A");
+letterCount(jsHistory, "a");
 
 // ------------------------------------------
 
